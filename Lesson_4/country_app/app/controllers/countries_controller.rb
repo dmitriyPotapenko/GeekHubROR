e7ac8a29b376c9c3acbbe1593c0c57121e54722e
dmitriyPotapenko <potap1692@gmail.com>
2017-10-26 05:39:34 -0700

    second from ten

---
+++ Lesson_4/country_app/app/controllers/countries_controller.rb
@@ -0,0 +1,37 @@
+class CountriesController < ApplicationController
+
+	def index
+    @countries = Country.all  
+  end
+
+  def new
+  	@countries = Country.new
+  end
+
+  def create 
+  	@countries = Country.create(params.require(:country).permit(:name, :population))
+
+  	redirect_to @countries
+  end
+
+  def show
+  	@country = Country.find(params[:id])
+  end
+
+  def edit
+  	@country = Country.find(params[:id])
+  end
+
+  def update
+  	@country = Country.update(params.require(:country).permit(:name, :population))
+
+  	redirect_to countries_path
+  end
+
+  def destroy
+  	@country = Country.find(params[:id]).destroy
+
+ 		redirect_to countries_path
+  end
+
+end	
